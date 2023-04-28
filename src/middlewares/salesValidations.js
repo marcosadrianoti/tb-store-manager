443642d41acabaf2563aeb25cda8172347e2ab7d
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-04-27 21:56:08 -0300

    Req 06

---
+++ src/middlewares/salesValidations.js
@@ -0,0 +1,62 @@
+const productModel = require('../models/productModel');
+
+const haveProductId = (req, res, next) => {
+  const sales = req.body;
+  let isProductIdHere = true;
+  sales.forEach((sale) => {
+    if (Object.keys(sale).includes('productId') === false) {
+      isProductIdHere = false;
+    }
+  });
+  if (!isProductIdHere) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
+
+  next();
+};
+
+const haveQuantity = (req, res, next) => {
+  const sales = req.body;
+  let isQuantityHere = true;
+  sales.forEach((sale) => {
+    if (Object.keys(sale).includes('quantity') === false) {
+      isQuantityHere = false;
+    }
+  });
+  if (!isQuantityHere) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+
+  next();
+};
+
+const qtIsGreaterThanZero = (req, res, next) => {
+  const sales = req.body;
+  let greaterThanZero = true;
+  sales.forEach((sale) => {
+    if (sale.quantity <= 0) {
+      greaterThanZero = false;
+    }
+  });
+  if (!greaterThanZero) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+
+  next();
+};
+
+const isThereProductId = async (req, res, next) => {
+  const sales = req.body;
+  let thereIs = true;
+  const allProducts = await productModel.getAllProducts();
+  sales.forEach((sale) => {
+    thereIs = allProducts.some((product) => product.id === sale.productId);
+  });
+  if (!thereIs) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+  next();
+};
+
+module.exports = { haveProductId, haveQuantity, qtIsGreaterThanZero, isThereProductId };
--- src/routes/salesRoutes.js
+++ src/routes/salesRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const saleController = require('../controllers/saleController');
+const {
+  haveProductId,
+  haveQuantity,
+  qtIsGreaterThanZero,
+  isThereProductId,
+} = require('../middlewares/salesValidations');
 
 const route = express.Router();
 
-// route.get('/', productController.getAllProducts);
-
-// route.get('/:id', productController.getById);
-
-route.post('/', saleController.insertNewSale);
+route.post('/',
+  haveProductId,
+  haveQuantity,
+  qtIsGreaterThanZero,
+  isThereProductId,
+  saleController.insertNewSale);
 
 module.exports = route;
